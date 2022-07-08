36bdb2bf1e3ba2fdc087674f68c0a1bb050038f8
DAZELPRO <dazelpro@gmail.com>
2022-07-08 11:43:33 +0700

    success login google debug and release

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,18 +1,58 @@
+import {GoogleSignin, statusCodes} from '@react-native-google-signin/google-signin';
 import React from 'react';
-import {View, Text, TouchableOpacity, ToastAndroid, StatusBar} from 'react-native';
+import {
+    View,
+    Text,
+    TouchableOpacity,
+    ToastAndroid,
+    StatusBar,
+} from 'react-native';
+
+GoogleSignin.configure({
+    webClientId:
+        '53061255389-m1q22e861121jn79k9me2af05uq8tuk1.apps.googleusercontent.com',
+    offlineAccess: true,
+});
 
 const Login = ({navigation}) => {
-    const onLoginGoogle = () => {
-        ToastAndroid.show('Login google sedang diproses', ToastAndroid.SHORT);
-        navigation.navigate('Dashboard');
+
+    const onLoginGoogle = async () => {
+        try {
+            await GoogleSignin.hasPlayServices();
+            const userInfo = await GoogleSignin.signIn();
+            let data = {
+                user: {
+                    email: userInfo['user']['email'],
+                    id: userInfo['user']['id'],
+                    name: userInfo['user']['name'],
+                    photo: userInfo['user']['photo'],
+                    provider: 'Google',
+                },
+            };
+            // loginProcess(data);
+            ToastAndroid.show(`Selamat datang ${data.user.name}`, ToastAndroid.SHORT);
+        } catch (error) {
+            if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+                ToastAndroid.show('Login google batal', ToastAndroid.SHORT);
+            } else if (error.code === statusCodes.IN_PROGRESS) {
+                console.log('e 2');
+            } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+                ToastAndroid.show('Sistem tidak support', ToastAndroid.SHORT);
+            } else {
+                console.log(error);
+            }
+        }
     };
+
     const onLoginFacebook = () => {
         ToastAndroid.show('Login facebook sedang diproses', ToastAndroid.SHORT);
         navigation.navigate('Dashboard');
     };
+
     const onLoginEmail = () => {
         ToastAndroid.show('Fitur ini belum tersedia', ToastAndroid.SHORT);
     };
+    
     return (
         <>
             <StatusBar
@@ -38,7 +78,9 @@ const Login = ({navigation}) => {
                         borderRadius: 5,
                     }}
                     onPress={onLoginGoogle}>
-                    <Text style={{fontSize: 16}}>Login with Google</Text>
+                    <Text style={{color: '#000', fontSize: 16}}>
+                        Login with Google
+                    </Text>
                 </TouchableOpacity>
                 <TouchableOpacity
                     style={{
